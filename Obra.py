dca65d98474f003e7ff4e8a5fd4c9abaf7b2e4b8
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-06 16:17:44 -0400

    Update Obra.py

--- Obra.py
+++ Obra.py
@@ -10,10 +10,12 @@ class Obra:
         anio_creacion 
         imagen_obra (str)        
     """
-    def __init__(self, titulo, artista, id_departamento, tipo, anio_creacion, imagen_obra):
+    def __init__(self, titulo, artista, nacionalidad_artista, fecha_nacimeinto_art, fecha_muerte_art, tipo, anio_creacion, imagen_obra):
         self.titulo = titulo
         self.artista = artista
-        self.id_departamento = id_departamento
+        self.nacionalidad_artista = nacionalidad_artista
+        self.fecha_na_art = fecha_nacimeinto_art
+        self.fecha_mue_art= fecha_muerte_art
         self.tipo = tipo
         self.anio_creacion = anio_creacion
         self.imagen_obra = imagen_obra
@@ -21,7 +23,10 @@ class Obra:
     def show(self):
         print(f"""Título: {self.titulo}
 Nombre del artista: {self.artista}
-ID del departamento: {self.id_departamento}
+Nacionalidad del artista: {self.nacionalidad_artista}
+Fecha de naciomiento del artista: {self.fecha_na_art}
+Fecha de muerte del artista: {self.fecha_mue_art}
 Tipo de obra: {self.tipo}
 Año de creación: {self.anio_creacion}
 Imagen de la obra: {self.imagen_obra}""")
+
