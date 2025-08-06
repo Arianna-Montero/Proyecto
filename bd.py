d1ecca28c877d7afab6167f2702cb0a191fb1aaf
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-06 01:54:41 -0400

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,3 +1,4 @@
+import Museo
 import requests
 """
 Acceder a los datos de las obras del museo
@@ -9,18 +10,13 @@ deparments = requests.get("https://collectionapi.metmuseum.org/public/collection
 #search_obras_departamento = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=[departmentId]&q=cat") 
 search_obras_localidad = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/search?geoLocation=[geoLocation]&q=flowers") 
 search_obras_departamento = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=[departmentId]&q=cat") 
-objectID = 1
-object = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{objectID}") #400
+
+#object = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects") #400
 
 #objects_datos = objects.json()
 #object_datos = object.json()
-#deparments_datos = deparments.json()
-#search_datos = search.json()
-print(f"Código de estado: {object.status_code}") #esto es para probar si da error
+deparments_datos= deparments.json()
+#search_localidad = search_obras_localidad.json()
+#print(f"Código de estado: {object.status_code}")
 #print(f"Texto de la respuesta:\n{objects.text}")
-
-objects_datos = objects.json()
-object_datos = object.json()
-deparments_datos = deparments.json()
-search_datos = search.json()
 
