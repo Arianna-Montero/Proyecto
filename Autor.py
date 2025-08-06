cc764f5b2d904cb643b9fab944c04466e59afba3
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-05 21:49:57 -0400

    Update Autor.py

--- Autor.py
+++ Autor.py
@@ -13,3 +13,8 @@ class Autor:
         self.nacionalidad_artista = nacionalidad_artista
         self.fecha_nacimiento = fecha_nacimiento
         self.fecha_muerte = fecha_muerte
+    def show(self):
+        print(f"""Artista: {self.nombre_artista}
+Nacionalidad: {self.nacionalidad_artista}
+Fecha de nacimiento: {self.fecha_nacimiento}
+Fecha de muerte: {self.fecha_muerte}""")
