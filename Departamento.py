131116f1314889ae097467d69aa75cd5666bd42e
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-06 01:53:32 -0400

    Update Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -1,11 +1,8 @@
 class Departamento:
-    """
-    Crear departamentos del museo
-    
-    Atributos:
-        nombre_departamento (str)
-        id         
-    """ 
+
     def __init__(self, nombre_departamento, id):
         self.nombre_departamento = nombre_departamento
         self.id = id
+    def show (self):
+        print (f"""Nombre del departamento: {self.nombre_departamento}.
+ID: {self.id}""")
