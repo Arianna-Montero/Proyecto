428f5252bfff36b4b2016400e1ac3ad6dda2c08c
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-06 01:55:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Museo import Museo
-from bd import objects, object, deparments, search_obras_localidad, search_obras_departamento
+from bd import objects, deparments #object, search_obras_localidad, search_obras_departamento
 def main():
     museo=Museo(objects, object, deparments) #search_obras_localidad, search_obras_departamento)
     museo.start()
