dae4db64c2f64ad8855c170010c6976bf14fb205
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-07 12:42:38 -0400

    Update Obra.py

--- Obra.py
+++ Obra.py
@@ -10,23 +10,23 @@ class Obra:
         anio_creacion 
         imagen_obra (str)        
     """
-    def __init__(self, titulo, artista, nacionalidad, fecha_nacimiento, fecha_muerte, tipo, anio_creacion, imagen_obra):
+    def __init__(self, titulo, artista, nacionalidad_artista, fecha_nacimeinto_art, fecha_muerte_art, tipo, anio_creacion):
         self.titulo = titulo
         self.artista = artista
-        self.nacionalidad = nacionalidad          
-        self.fecha_nacimiento = fecha_nacimiento  
-        self.fecha_muerte = fecha_muerte          
+        self.nacionalidad_artista = nacionalidad_artista
+        self.fecha_na_art = fecha_nacimeinto_art
+        self.fecha_mue_art= fecha_muerte_art
         self.tipo = tipo
         self.anio_creacion = anio_creacion
-        self.imagen_obra = imagen_obra
+        
 
     def show(self):
         print(f"""Título: {self.titulo}
 Nombre del artista: {self.artista}
-Nacionalidad: {self.nacionalidad}
-Fecha de nacimiento: {self.fecha_nacimiento}
-Fecha de muerte: {self.fecha_muerte}
+Nacionalidad del artista: {self.nacionalidad_artista}
+Fecha de nacimiento del artista: {self.fecha_na_art}
+Fecha de muerte del artista: {self.fecha_mue_art}
 Tipo de obra: {self.tipo}
-Año de creación: {self.anio_creacion}
-Imagen de la obra: {self.imagen_obra}""")
+Año de creación: {self.anio_creacion}""")
         
+
