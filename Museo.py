e20ea30f3a779983dcfb7012ced917d045ac1c38
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-07 11:40:12 -0400

    Update Museo.py

--- Museo.py
+++ Museo.py
@@ -105,8 +105,8 @@ Elija una opción de búqueda de obras:
         #self.obras.append(Obra(obras["title"], obras["artistDisplayName"], departamento_encontrado, obras["objectName"], obras["objectBeginDate"], obras["primaryImage"]))
         #deberiamos borrar todo esto
     
-def mostrar_obra(self):
-    
+    def mostrar_obra(self):
+        
         obraID = int(input("Ingrese el id de la obra que desea ver: "))
         print()
         try:
@@ -116,11 +116,11 @@ def mostrar_obra(self):
             for obra in self.obras:
                 obra.show()
                 print()   
-          
+            
         except requests.exceptions.RequestException as e:
-            print("ID no encontrado")
-            pass
-        
+                print("ID no encontrado")
+                pass
+            
         id = input("""¿Mostrar imagen?
 -> Si
 -> No                           
@@ -131,12 +131,12 @@ def mostrar_obra(self):
              
             if obra_m.get("primaryImage"):
                 nombre_archivo_destino = guardar_imagen_desde_url(obra_m["primaryImage"], f"obra_{obraID}")
-                    
                 img = Image.open(nombre_archivo_destino)
                 img.show()                       
+                
                                         
     def buscar_obra_departamentoID(self):
-         obras_departamento = int(input("Escriba el ID del departamento para ver las obras: "))
+        obras_departamento = int(input("Escriba el ID del departamento para ver las obras: "))
         print()
         try:
             search_obras_departamento = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId={obras_departamento}&q=cat") 
@@ -153,65 +153,64 @@ def mostrar_obra(self):
                     except requests.exceptions.RequestException as e:
                         break
         except requests.exceptions.RequestException as e:
-             pass
+            pass
             #self.mostrar_obra.append(Obras(obra.get("objectID"), obra.get("title"), obra.get("artistDisplayName")))
             #for obra in self.obras:
                 #obra.show()
                 #print()
           
     def buscar_obra_nacionalidad(self):
-        
-       nacionalidad= input("Ingrese la nacionalidad del autor: ")
-            print()
-            try: 
-                search_obras_localidad = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q={nacionalidad}")
-                search_localidad = search_obras_localidad.json() 
-                for id in search_localidad["objectIDs"]:
-                    try:
-                        object_list = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{id}", timeout=20) 
-                        obra= object_list.json()
-                        self.obras.append(Obras(obra.get("objectID"), obra.get("title"), obra.get("artistDisplayName")))
-                        for obra in self.obras:
-                            obra.show()
-                            print()
-                    except requests.exceptions.RequestException as e:
-                        print("No se pudo encontrar las obras")
-                        break
-            except requests.exceptions.RequestException as e:
-                    print("No se puso encontrar la nacionalidad")
-                    pass
+        nacionalidad= input("Ingrese la nacionalidad del autor: ")
+        print()
+        try: 
+            search_obras_localidad = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?artistOrCulture=true&q={nacionalidad}")
+            search_localidad = search_obras_localidad.json() 
+            for id in search_localidad["objectIDs"]:
+                try:
+                    object_list = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{id}", timeout=20) 
+                    obra= object_list.json()
+                    self.obras.append(Obras(obra.get("objectID"), obra.get("title"), obra.get("artistDisplayName")))
+                    for obra in self.obras:
+                        obra.show()
+                        print()
+                except requests.exceptions.RequestException as e:
+                    print("No se pudo encontrar las obras")
+                    break
+        except requests.exceptions.RequestException as e:
+                print("No se puso encontrar la nacionalidad")
+                pass
             #self.nacionalidades.append(Obras(obra.get("objectID"), obra.get("title"), obra.get("artistDisplayName")))     
             #for obra in self.obras:
                 #obra.show()
                 #print()
     
-def mostrar_nacionalidades(self):
-        for nacionalidad in self.nacionalidades:
-            nacionalidad.show()               
-    
+    def mostrar_nacionalidades(self):
+            for nacionalidad in self.nacionalidades:
+                nacionalidad.show()               
+        
     def autores_abecedario(self):
         """
         Obtener autores alfabeticamente.
         """
         letras = "abcdefghijklmnopqrstuvwxyz"
         autores = []
-        
-        for letra in letras:
             
+        for letra in letras:
+                
             buscar_letra = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/search?q={letra}")
             id_letra = buscar_letra.json()
             ids_obras = id_letra.get("objectIDs")[:1]
-            
+                
             for id_obra in ids_obras:
-                
+                    
                 buscar_obra = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{id_obra}")
-                obra = buscar_obra.json()
+            obra = buscar_obra.json()
                 
-                buscar_nombre = obra.get("artistDisplayName")
-                
-                if buscar_nombre not in autores:
-                    autores.append(buscar_nombre)
-        
+            buscar_nombre = obra.get("artistDisplayName")
+                    
+            if buscar_nombre not in autores:
+                autores.append(buscar_nombre)
+            
         return autores
             
             
@@ -228,14 +227,14 @@ def mostrar_nacionalidades(self):
                     obra= object_list.json()
                     self.obras.append(Obras(obra.get("objectID"), obra.get("title"), obra.get("artistDisplayName")))
                     for obra in self.obras:
-                        obra.show()
-                        print()
-        #print(search_autor) #<-código ari
+                            obra.show()
+                            print()
+            #print(search_autor) #<-código ari
                 except requests.exceptions.RequestException as e:
-                    break
+                        break
         except requests.exceptions.RequestException as e:
-                print("No se puso encontrar al autor")
-                pass
+            print("No se puso encontrar al autor")
+            pass
    
           
             #self.obras.append(Obras(obra["objectIDs"]))
@@ -252,4 +251,5 @@ def mostrar_nacionalidades(self):
                 
         #        except requests.exceptions.RequestException as e:
         #            break
+
 
