7e5dc2bd0d88306dfeb44b57545fb84d7b34ee1f
VicP29 <v.pereira@correo.unimet.edu.ve>
2025-08-07 11:41:15 -0400

    Update Autor.py

--- Autor.py
+++ Autor.py
@@ -3,18 +3,10 @@ class Autor:
     Crear autores de las obras
     
     Atributos:
-        nombre_artista (str)
-        nacionalidad_artista (str) 
-        fecha_nacimiento 
-        fecha_muerte        
+        nombre_artista (str)       
     """    
-    def __init__(self, nombre_artista, nacionalidad_artista, fecha_nacimiento, fecha_muerte):
+    def __init__(self, nombre_artista):
         self.nombre_artista = nombre_artista
-        self.nacionalidad_artista = nacionalidad_artista
-        self.fecha_nacimiento = fecha_nacimiento
-        self.fecha_muerte = fecha_muerte
     def show(self):
-        print(f"""Artista: {self.nombre_artista}
-Nacionalidad: {self.nacionalidad_artista}
-Fecha de nacimiento: {self.fecha_nacimiento}
-Fecha de muerte: {self.fecha_muerte}""")
+        print(f"""Artista: {self.nombre_artista}""")
+
