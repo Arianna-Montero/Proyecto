42084e6adbd761ea40a482916cdb5d9aed1c7f73
Arianna <a.montero@correo.unimet.edu.ve>
2025-08-07 18:21:34 -0400

    Nacionalidades.py

--- Nacionalidades.py
+++ Nacionalidades.py
@@ -1,5 +1,12 @@
 class Nacionalidad:
+    """
+    Crea las nacionalidades de los autores.
+    
+    Atributos:
+        nacionalidad
+    """
     def __init__(self, nacionalidad):
         self.nacionalidad = nacionalidad
+        
     def show(self):
         print(f"{self.nacionalidad}")
