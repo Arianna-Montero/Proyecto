1e899e6a6ab3827ecf044de2b140d030d71dfe22
Arianna <a.montero@correo.unimet.edu.ve>
2025-08-08 08:26:03 -0400

    Obras.py

--- Obras.py
+++ Obras.py
@@ -1,10 +1,22 @@
 class Obras:
     def __init__(self, id, titulo, autor):
+        """
+        Obras.
+        
+        Args:
+            id (int): ID de la obra.
+            titulo (str): Título de la obra.
+            autor (str): Nombre del autor.
+        """
         self.id= id
         self.tiutlo= titulo
         self.autor= autor
 
     def show(self):
+        """
+        Mostrar obras.
+        """
         print(f"""ID de la obras: {self.id}
 Titulo: {self.tiutlo}
 Autor: {self.autor}""")
+        
