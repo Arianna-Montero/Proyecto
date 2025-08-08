661a067d0c47dd29d495057bf35c68fbd62aeff8
Arianna <a.montero@correo.unimet.edu.ve>
2025-08-08 08:21:44 -0400

    Autor.py

--- Autor.py
+++ Autor.py
@@ -1,12 +1,17 @@
 class Autor:
-    """
-    Crear autores de las obras
-    
-    Atributos:
-        nombre_artista (str)       
-    """    
+  
     def __init__(self, nombre_artista):
+        """
+        Nombre de autor.
+        
+        Args:
+            nombre_artista (str): Nombre del autor.
+        """
         self.nombre_artista = nombre_artista
         
     def show(self):
+        """
+        Mostrar nombre del autor.
+        """
         print(f"Artista: {self.nombre_artista}")
+        
