cb357b878b890b82ab551dd5683b4d3b726e08d3
Arianna <a.montero@correo.unimet.edu.ve>
2025-08-08 08:22:22 -0400

    Departamento.py

--- Departamento.py
+++ Departamento.py
@@ -1,8 +1,19 @@
 class Departamento:
 
     def __init__(self, nombre_departamento, id):
+        """
+        Nombre de departamento.
+        
+        Args:
+            nombre_departamento (str): Nombre del departamento.
+            id : ID del departamento.
+        """
         self.nombre_departamento = nombre_departamento
         self.id = id
+        
     def show (self):
+        """
+        Mostrar nombre del departamento.
+        """
         print (f"""Nombre del departamento: {self.nombre_departamento}.
 ID: {self.id}""")
